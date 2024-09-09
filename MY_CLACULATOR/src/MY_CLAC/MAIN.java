11303b33e97cb484a5e4189208603454e450c843
Ahmedsaeed1104 <168998929+Ahmedsaeed1104@users.noreply.github.com>
2024-09-09 07:18:58 +0300

    Add files via upload

---
+++ MY_CLACULATOR/src/MY_CLAC/MAIN.java
@@ -0,0 +1,10 @@
+package MY_CLAC;
+
+public class MAIN {
+
+	public static void main(String[] args) {
+		
+		GUI G = new GUI();
+	}
+
+}
---
+++ MY_CLACULATOR/src/MY_CLAC/PANEL.java
@@ -0,0 +1,6 @@
+package MY_CLAC;
+import javax.swing.*;
+import java.awt.*;
+public class PANEL  {
+
+}
